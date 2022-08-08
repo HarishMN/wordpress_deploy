d566197339a29c2e167308386d7ed67f11384b45
HarishMN <harish.navarathna.hn@gmail.com>
2022-08-08 15:23:59 +0530

    installed all depandance

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -10,4 +10,16 @@ echo"installed nginx"
 sudo systemctl start nginx
 
 sudo systemctl enable nginx
+
+sudo apt install mariadb-server -y
+
+sudo  systemctl start mariadb
+sudo systemctl enable mariadb
+
+sudo apy install php -y
+
+sudo apt install php-mysql php-gd php-common php-mbstring php-curl php-cli -y
+sudo systemctl restart nginx
+
+sudo apt install php-fpm -y
 EOF
