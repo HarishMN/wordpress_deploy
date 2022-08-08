bd83fccd9d94e4ea6f6ed48c6b0f8f7e6be51640
HarishMN <harish.navarathna.hn@gmail.com>
2022-08-08 16:12:52 +0530

    removed unzip added tar file

--- Scripts/deploy.sh
+++ Scripts/deploy.sh
@@ -5,33 +5,34 @@ echo "directory created"
 timeout 200 ssh -t -i ~/onekingharry.pem ubuntu@65.1.64.90 <<EOF
 
 echo "sudo apt install nginx -y"
-echo"installed nginx"
+echo "installed nginx"
 
 sudo systemctl start nginx
 
 sudo systemctl enable nginx
 
-echo"sudo apt install mariadb-server -y"
+echo "sudo apt install mariadb-server -y"
 
 sudo  systemctl start mariadb
-sudo systemctl enable mariadb
+sudo  systemctl enable mariadb
 
-echo"sudo apy install php -y"
+echo "sudo apy install php -y"
 
-echo"sudo apt install php-mysql php-gd php-common php-mbstring php-curl php-cli -y"
+echo "sudo apt install php-mysql php-gd php-common php-mbstring php-curl php-cli -y"
 sudo systemctl restart nginx
 
-echo"sudo apt install php-fpm -y"
+echo "sudo apt install php-fpm -y"
 
-wget https://wordpress.org/latest.zip
+ wget -c http://wordpress.org/latest.tar.gz
 
-echo"sudo apt install unzip -y"
+ echo "downloaded wordpress file"  
+ 
+ tar -xzvf latest.tar.gz
+ echo "unziping tar file"
 
-unzip latest.zip
-echo"unziped latest.zip"
+ rsync -av wordpress/* /var/www/html/
+ echo "synced  wordpress  to html"  
 
-sudo mv wordpress/* /var/www/html
-echo"moved wordpress to html"
 cd /var/www/html
 echo"in the folder"
 EOF
